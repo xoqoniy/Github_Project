316a7902cedbfef8dd18d8b9086c7af4d8dd0018
Odina Abduganiyeva <125026739+odinaabduganiyeva03@users.noreply.github.com>
2023-03-17 16:30:39 +0300

    added

--- Domain/Entities/Languages.cs
+++ Domain/Entities/Languages.cs
@@ -1,12 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Domain.Entities
+﻿namespace Domain.Entities
 {
-    internal class Languages
+    public class Languages
     {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public long RepositoryId { get; set; }
+
     }
 }
