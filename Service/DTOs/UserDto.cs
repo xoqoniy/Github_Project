bbe2ddb8bc83db8d17badd94ba08702a64be6977
Odina Abduganiyeva <125026739+odinaabduganiyeva03@users.noreply.github.com>
2023-03-18 04:26:41 +0300

    users-added

--- Service/DTOs/UserDto.cs
+++ Service/DTOs/UserDto.cs
@@ -1,12 +1,23 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Domain.Entities;
 
 namespace Service.DTOs
 {
-    internal class UserDto
+    public class UserDto
     {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Username { get; set; }
+        public string Description { get; set; }
+
+        public static explicit operator UserDto(User user)
+        {
+            return new UserDto()
+            {
+                FirstName = user.FirstName,
+                LastName = user.LastName,
+                Username = user.Username,
+                Description = user.Description
+            };
+        }
     }
 }
